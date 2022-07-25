02b7001a591d192d18c19ec8cab4cf689cb8b815
Rustam Hajrullin <rust02war53@gmail.com>
2022-07-26 03:19:37 +0500

    help screen

---
+++ app/src/main/java/com/example/simbirsoftpracticeapp/Category.kt
@@ -0,0 +1,7 @@
+package com.example.simbirsoftpracticeapp
+
+data class Category(
+    val id: Int,
+    val title: String,
+    val imgResId: Int
+)
--- app/src/main/java/com/example/simbirsoftpracticeapp/MainActivity.kt
+++ app/src/main/java/com/example/simbirsoftpracticeapp/MainActivity.kt
@@ -2,15 +2,54 @@ package com.example.simbirsoftpracticeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.simbirsoftpracticeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportFragmentManager.beginTransaction().run {
-            add(R.id.fragment_container_view, ProfileFragment())
+            add(R.id.fragment_container_view, HelpFragment())
             commit()
+        }
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        binding.bottomNavigationView.selectedItemId = R.id.navigation_search
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_news -> {
+                    true
+                }
+                R.id.navigation_search -> {
+                    true
+                }
+                R.id.navigation_help -> {
+                    supportFragmentManager.beginTransaction().run {
+                        replace(R.id.fragment_container_view, HelpFragment())
+                        commit()
+                    }
+                    true
+                }
+                R.id.navigation_history -> {
+                    true
+                }
+                R.id.navigation_profile -> {
+                    supportFragmentManager.beginTransaction().run {
+                        replace(R.id.fragment_container_view, ProfileFragment())
+                        commit()
+                    }
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
