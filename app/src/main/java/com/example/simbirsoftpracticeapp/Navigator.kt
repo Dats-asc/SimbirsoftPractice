446a57230016c20e065472b5e44e2abb4aee9105
Rustam Hajrullin <rust02war53@gmail.com>
2022-08-05 14:38:50 +0500

    auth screen layout

---
+++ app/src/main/java/com/example/simbirsoftpracticeapp/Navigator.kt
@@ -0,0 +1,6 @@
+package com.example.simbirsoftpracticeapp
+
+interface Navigator {
+
+    fun onAuthSuccesses()
+}
---
+++ app/src/main/java/com/example/simbirsoftpracticeapp/auth/AuthFragment.kt
@@ -0,0 +1,69 @@
+package com.example.simbirsoftpracticeapp.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import com.example.simbirsoftpracticeapp.Navigator
+import com.example.simbirsoftpracticeapp.databinding.FragmentAuthBinding
+
+class AuthFragment : Fragment() {
+
+    private lateinit var binding: FragmentAuthBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentAuthBinding.inflate(inflater, container, false).let {
+        binding = FragmentAuthBinding.inflate(inflater, container, false)
+        binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(EMAIL_STRING, binding.etEmail.text.toString())
+        outState.putString(PASSWORD_STRING, binding.etPassword.text.toString())
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        binding.etEmail.setText(savedInstanceState?.getString(EMAIL_STRING))
+        binding.etPassword.setText(savedInstanceState?.getString(PASSWORD_STRING))
+    }
+
+    private fun init() {
+        with(binding) {
+            toolbar.setNavigationOnClickListener {
+                requireActivity().onBackPressed()
+            }
+
+            etEmail.doOnTextChanged { text, start, before, count ->
+                onTextChanged()
+            }
+
+            etPassword.doOnTextChanged { text, start, before, count ->
+                onTextChanged()
+            }
+
+            btnSignIn.setOnClickListener {
+                (requireActivity() as Navigator).onAuthSuccesses()
+            }
+        }
+    }
+
+    private fun onTextChanged() {
+        binding.btnSignIn.isEnabled = binding.etEmail.text.length >= 6 && binding.etPassword.text.length >= 6
+    }
+
+    companion object {
+        private const val EMAIL_STRING = "EMAIL_STRING"
+        private const val PASSWORD_STRING = "PASSWORD_STRING"
+    }
+}
